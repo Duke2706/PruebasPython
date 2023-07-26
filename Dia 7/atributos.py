492f716a3ffc1d134b2240aa8cb581fcb88a1463
Carlos <carlos.antizar@gmail.com>
2023-07-26 22:28:09 +0200

    Proyecto dia 6

---
+++ Dia 7/atributos.py
@@ -0,0 +1,13 @@
+class Pajaro:
+
+    alas = True
+    alas = True
+    def __init__(self, color, especie):
+        self.color = color
+        self.especie = especie
+
+
+mi_pajaro = Pajaro("Blanco", "Loro")
+
+print(f"Mi pajaro es un {mi_pajaro.especie} y es de color {mi_pajaro.especie} y tiene {mi_pajaro.alas}")
+
